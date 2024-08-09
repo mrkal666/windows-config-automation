a0a6eeebbd52f92b5f4821197025e147bb0de25a
Mr. Kal <kalkkuna05@gmail.com>
2024-08-09 17:51:22 +0300

    Added the rest of the script

--- install.ps1
+++ install.ps1
@@ -1,8 +1,41 @@
-$wingetPackages = @(
-    'git.git',
-    'Microsoft.VisualStudioCode',
-    'Google.Chrome'
+param (
+    [ValidateSet("dev-laptop", "laptop", "dev-desktop", "desktop")]
+    [string]$ComputerType = "dev-laptop"
 )
+
+$wingetPackages = @(
+    'Git.Git',
+    'Microsoft.VisualStudioCode',
+    'OpenJS.NodeJS.LTS',
+    'Google.Chrome',
+    '7zip.7zip',
+    'JanDeDobbeleer.OhMyPosh',
+    'Microsoft.PowerToys',
+    'Microsoft.WindowsTerminal',
+    'Mozilla.Firefox',
+    'Notepad++.Notepad++',
+    'VideoLAN.VLC',
+    'WinDirStat.WinDirStat',
+    'WinSCP.WinSCP',
+    'Discord.Discord',
+    'Valve.Steam',
+    'ShareX.ShareX',
+    'CodecGuide.K-LiteCodecPack.Full',
+    'th-ch.YouTubeMusic',
+    'GNU.Nano',
+    'Python.Python.3.12',
+    'GitHub.cli',
+    'SyncTrayzor.SyncTrayzor',
+    'Microsoft.Office',
+    'PuTTY.PuTTY'
+)
+
+$wingetPackagesDevDesktop = @(
+    'SyncTrayzor.SyncTrayzor',
+    'Microsoft.VisualStudio.2022.Community.Preview'
+)
+
+$dots_repo = "https://github.com/mrkal666/dots.git"
 
 # Check for winget
 if (-not(Get-Module -ListAvailable -Name Microsoft.Winget.Client)) {
@@ -17,24 +50,102 @@ if (-not(Get-Module -ListAvailable -Name Microsoft.Winget.Client)) {
           throw "Microsoft.Winget.Client was not found. Check that the Windows Package Manager PowerShell module was installed correctly."
         }
         Write-Output "Microsoft.Winget.Client was installed successfully"
+        Import-Module -Name Microsoft.Winget.Client
         Repair-WinGetPackageManager
     }
 } else {
     Write-Output "Microsoft.Winget.Client is already installed"
     Import-Module -Name Microsoft.Winget.Client
+    Repair-WinGetPackageManager
 }
 
-# Install winget packages
-foreach ($package in $wingetPackages) {
-    Write-Output "Installing package: $package"
-    $packageExists = Get-WinGetPackage -Id $package -Exact
-    if ($packageExists) {
-        Write-Output "Package $package is already installed"
-        continue
+try {
+    Import-Module DISM -UseWindowsPowerShell
+} catch {
+    throw "Cannot import DISM module"
+}
+
+Write-Output $ComputerType
+
+# Install Github CLI
+Write-Output "Installing package: GitHub.cli"
+    
+$listAppGitHub = winget list --exact --id "GitHub.cli"
+if (![String]::Join("", $listAppGitHub).Contains("GitHub.cli")) {
+    Write-host "Installing: GitHub.cli"
+    $status = Install-WinGetPackage -Id "GitHub.cli"
+    if ($status.Status -ne "Ok") {
+        Write-Host "Failed to install package: GitHub.cli"
     }
-    else {
-        Write-Output("test")
-        Install-WingetPackage -Id $package
+}
+else {
+    Write-host "Skipping: GitHub.cli (already installed)"
+}
+
+# Refresh PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+# Auth to GitHub
+# gh auth login
+# 
+# # Install winget packages
+# foreach ($package in $wingetPackages) {
+#     Write-Output "Installing package: $package"
+#     
+#     $listApp = winget list --exact --id $package
+#     if (![String]::Join("", $listApp).Contains($package)) {
+#         Write-host "Installing: " $package
+#         $status = Install-WinGetPackage -Id $package
+#         if ($status.Status -ne "Ok") {
+#             Write-Host "Failed to install package: " $package
+#         }
+#     }
+#     else {
+#         Write-host "Skipping: " $package " (already installed)"
+#     }
+# }
+
+# Refresh PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+# Install Dots
+if (Test-Path "$env:USERPROFILE/dots") {
+    Write-Output "User profile/dots directory exists"
+    
+    if(-not(Test-Path $profile -PathType Leaf)) {
+        Write-Output "Creating Profile"
+        New-Item $profile
+    }
+    if ($null -eq (Select-String -Path $profile -Pattern "oh-my-posh"))
+    {
+        Get-Content $env:USERPROFILE/dots/profile.ps1 >> $profile
     }
     
+} else {
+    Write-Output "User profile/dots directory does not exist"
+    git clone $dots_repo $env:USERPROFILE/dots
+    if(-not(Test-Path $profile -PathType Leaf)) {
+        New-Item $profile
+    }
+    if ($null -eq (Select-String -Path $profile -Pattern "oh-my-posh"))
+    {
+        Get-Content $env:USERPROFILE/dots/profile.ps1 >> $profile
+    }
 }
+
+# Install WSL
+Write-Output "Installing WSL"
+if($ComputerType -eq "dev-laptop" -or $ComputerType -eq "dev-desktop") {
+    wsl.exe --update
+    wsl.exe --install -d ubuntu --no-launch
+}
+
+# Install Features
+Write-Output "Installing Features"
+if($ComputerType -eq "dev-desktop") {
+    Write-Output "Installing Hyper-V"
+    Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All
+    Write-Output "Installing HypervisorPlatform"
+    Enable-WindowsOptionalFeature -FeatureName HypervisorPlatform
+}
+
